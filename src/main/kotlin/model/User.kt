85cd989d6a134acd51ddc449593062b6a0511bc9
antoine <aassali.dev@gmail.com>
2025-10-22 00:06:43 +0200

    Refactor `User` model: move `timeline` property to constructor

--- src/main/kotlin/model/User.kt
+++ src/main/kotlin/model/User.kt
@@ -1,7 +1,6 @@
 package model
 
-data class User(val name: String){
-    val timeline : Messages = mutableListOf()
+data class User(val name: String, val timeline: Messages = mutableListOf()){
 
     fun publish(message: Message) {
         timeline.add(message)
